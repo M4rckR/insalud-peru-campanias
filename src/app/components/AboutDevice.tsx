8e1802a44841921d0c64f76b72dcf7c62b7c67db
m4arck <marcromerogar4@gmail.com>
2025-06-20 12:04:55 -0500

    Device section Ready

--- src/app/components/AboutDevice.tsx
+++ src/app/components/AboutDevice.tsx
@@ -3,8 +3,7 @@ import Image from "next/image"
 
 export const AboutDevice = () => {
   return (
-    <div className="relative overflow-hidden">
-        {/* Wave superior */}
+    <div className="relative overflow-hidden mb-12 md:mb-32">
         <Image
             src={cdn("/campanas/vph-jesus-maria/assets/images/sections/main/wave-cyan-top.svg")}
             alt="Wave Cyan Top"
@@ -14,10 +13,10 @@ export const AboutDevice = () => {
         />
         
         <div className="-mt-1 bg-in-cyan relative">
-            <div className="container max-w-7xl mx-auto px-4 pb-20">
+            <div className="container max-w-6xl mx-auto px-4 md:pb-20">
                 <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 items-center">
                     <div className="lg:col-span-7 z-10">
-                        <h2 className="text-in-cyan-base text-center md:text-left text-4xl lg:text-5xl md:font-bold font-in-nunito font-black">
+                        <h2 className="text-in-cyan-base text-center pt-12 md:pt-16 lg:pt-0 lg:text-left text-3xl lg:text-4xl xl:text-5xl md:font-bold font-in-nunito font-black pb-2 md:pb-0 px-12 lg:px-0">
                             Equipos{" "}
                             <span className="text-in-blue">profesionales de primer nivel </span>{" "}
                             <span className="text-in-blue hidden md:inline">y Médicos especialistas</span>{" "}
@@ -33,25 +32,41 @@ export const AboutDevice = () => {
                                 alt="Equipo médico láser CO2"
                                 width={400}
                                 height={500}
-                                className="w-full h-auto relative z-10 mt-8 md:mt-0 lg:-mt-12 xl:-mt-20"
+                                className="hidden md:block w-full h-auto relative z-10 mt-8 md:mt-0 lg:-mt-12 xl:-mt-20"
                                 unoptimized
                             />                            
-                        </div>
+                            <Image
+                                src={cdn("/campanas/vph-jesus-maria/assets/images/sections/main/equipo-vph-mobile.png")}
+                                alt="Equipo médico láser CO2"
+                                width={400}
+                                height={500}
+                                className="w-full md:hidden max-w-64 mb-4"
+                                unoptimized
+                            />                            
+                        </div>  
                     </div>
                 </div>
             </div>
         </div>
-        
-        {/* Estadísticas con margen negativo */}
-        <div className="-mt-12 relative z-30">
-            <div className="container max-w-6xl mx-auto px-4">
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
+        <div className="mt-0 md:-mt-12 relative z-30">
+                <h2 className="text-right md:hidden px-4 text-4xl sm:3xl font-black font-in-nunito text-in-cyan-base bg-in-cyan pb-52 ">
+                    <div className="max-w-64 mx-auto">
+                        Médicos
+                        <div className="flex sm:block flex-col text-in-blue">
+                            <span className="">especialistas</span> {""}
+                            <span className="">certificados</span>
+                        </div>
+                    </div> 
+                    
+                    </h2>
+            <div className="container max-w-6xl mx-auto px-4 -mt-40 md:-mt-0">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 xl:gap-12">
                     <article className="bg-in-variant-cyan rounded-xl p-6 shadow-lg w-full min-w-0 max-w-[460px] md:max-w-none mx-auto md:mx-0">
-                        <div className="flex items-center justify-center gap-4">
+                        <div className="flex sm:flex-col lg:flex-row justify-start items-center md:justify-center gap-2 lg:gap-4">
                             <div className="text-4xl lg:text-5xl font-black text-in-blue font-in-nunito">
                                 +20000
                             </div>
-                            <div className="text-in-blue">
+                            <div className="text-in-blue sm:text-center lg:text-left">
                                 <p className="font-semibold leading-none">Atenciones
                                 <span className="block">satisfactorias</span></p>
                             </div>
@@ -59,11 +74,11 @@ export const AboutDevice = () => {
                     </article>
                     
                     <article className="bg-in-variant-cyan rounded-xl p-6 shadow-lg w-full min-w-0 max-w-[460px] md:max-w-none mx-auto md:mx-0">
-                        <div className="flex items-center justify-center gap-4">
+                        <div className="flex sm:flex-col lg:flex-row justify-start items-center md:justify-center gap-2 lg:gap-4">
                             <div className="text-4xl lg:text-5xl font-black text-in-blue font-in-nunito">
                                 +10
                             </div>
-                            <div className="text-in-blue">
+                            <div className="text-in-blue sm:text-center lg:text-left">
                                 <p className="font-semibold leading-none">Años de
                                 <span className="block">experiencia médica</span></p>
                             </div>
@@ -71,11 +86,11 @@ export const AboutDevice = () => {
                     </article>
                     
                     <article className="bg-in-variant-cyan rounded-xl p-6 shadow-lg w-full min-w-0 max-w-[460px] md:max-w-none mx-auto md:mx-0">
-                        <div className="flex items-center justify-center gap-4">
+                        <div className="flex sm:flex-col lg:flex-row justify-start items-center md:justify-center gap-2 lg:gap-4">
                             <div className="text-4xl lg:text-5xl font-black text-in-blue font-in-nunito">
                                 +5
                             </div>
-                            <div className="text-in-blue">
+                            <div className="text-in-blue sm:text-center lg:text-left">
                                 <p className="font-semibold leading-none">Sedes de atención
                                 <span className="block">en latinoamérica</span></p>
                             </div>
