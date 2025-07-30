f292880a7de462e3a86fadf7a5ad14a3c0d83650
M4rckR <marcromerogar4@gmail.com>
2025-07-30 10:31:49 -0500

    Google tag manager corregido v3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "../styles/globals.css";
 
 import { Toaster } from "@/components/ui/sonner";
 import GTMPageviewTracker from "@/app/GTMPageviewTracker";
+import Script from "next/script";
 
 const nunito = Nunito({
   variable: "--font-nunito",
@@ -65,14 +66,16 @@ export default function RootLayout({
         suppressHydrationWarning
       >
       <GTMPageviewTracker />
-      <noscript>
-        <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PR66DQ7B"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-        ></iframe>
-      </noscript>
+
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PR66DQ7B');
+          `}
+      </Script>
       <noscript>
         <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-PR66DQ7B"
